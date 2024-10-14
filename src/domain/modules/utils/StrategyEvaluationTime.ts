e7783f0db4617a450cb092e79bf27a6c77b8f52d
Lucas <luckericksen@gmail,com>
2024-10-14 10:29:50 -0300

    fix answercard evaluation

--- src/domain/modules/utils/StrategyEvaluationTime.ts
+++ src/domain/modules/utils/StrategyEvaluationTime.ts
@@ -1,8 +1,13 @@
-export const evalStrategy = (times: number) => ({
+export const evalStrategy = (times: number, evaluation: string) => ({
   'Very Hard': 60,
   Hard: 5 * 60,
-  Nomal: 60 * 30 * (times + 1) < 259200 ? 60 * 30 * (times + 1) : 259200,
+  Nomal:
+    times > 1 && evaluation === 'Normal' && 60 * 30 * (times + 1) < 259200
+      ? 60 * 30 * (times + 1)
+      : 259200,
   Easy:
+    times > 1 &&
+    evaluation === 'Easy' &&
     60 * 60 * 24 * 3 * (times + 1) < 7776000
       ? 60 * 60 * 24 * 3 * (times + 1)
       : 7776000,
