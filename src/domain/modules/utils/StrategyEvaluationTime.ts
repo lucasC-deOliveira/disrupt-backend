d5f9c01563503afa4ab5d3b45338556d4d702f58
lucas <luckericksen@gmail.com>
2025-02-06 14:24:12 -0300

    fix wrong card evaluation

--- src/domain/modules/utils/StrategyEvaluationTime.ts
+++ src/domain/modules/utils/StrategyEvaluationTime.ts
@@ -2,13 +2,11 @@ export const evalStrategy = (times: number, evaluation: string) => ({
   'Very Hard': 60,
   Hard: 5 * 60,
   Normal:
-    times > 1 && evaluation === 'Normal' && 60 * 30 * (times + 1) < 259200
-      ? 60 * 30 * (times + 1)
+    60 * 30 * (times || 1) < 259200
+      ? 60 * 30 * (times || 1)
       : 259200,
   Easy:
-    times > 1 &&
-    evaluation === 'Easy' &&
-    60 * 60 * 24 * 3 * (times + 1) < 7776000
-      ? 60 * 60 * 24 * 3 * (times + 1)
+    60 * 60 * 24 * 3 * (times || 1) < 7776000
+      ? 60 * 60 * 24 * 3 * (times || 1)
       : 7776000,
 });
