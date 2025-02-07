a4737a092275a18e1bde08c09412f01e1d190c57
lucas <luckericksen@gmail.com>
2025-02-07 11:49:58 -0300

    remove socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { HttpModule } from './infra/presentation/http/http.module';
-import { WebsocketsModule } from './infra/presentation/websockets/websockets.module';
 
 @Module({
-  imports: [HttpModule, WebsocketsModule],
+  imports: [HttpModule, ],
   controllers: [],
   providers: [],
 })
--- src/infra/presentation/websockets/websockets.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SyncGateway } from './sync.gateway';
-import { DeckService } from 'src/infra/services/deck.service';
-import { PrismaService } from 'src/database/prisma/prisma.service';
-
-@Module({
-  providers: [SyncGateway, DeckService, PrismaService],
-})
-export class WebsocketsModule {}
