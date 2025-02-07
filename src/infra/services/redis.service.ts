54a5b33e188b8e654cde22327b551ecbe2960552
lucas <luckericksen@gmail.com>
2025-02-07 15:47:30 -0300

    add redis password

--- src/infra/services/redis.service.ts
+++ src/infra/services/redis.service.ts
@@ -10,6 +10,7 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
             host: process.env.REDIS_HOST,
             port: parseInt(process.env.REDIS_PORT),
             db: parseInt(process.env.REDIS_DB),
+            password: process.env.REDIS_PASSWORD,
             
             // Outras configurações, se necessário.
         });
